5e0976dc117077259046e79aba8c4cb0048808d0
Collins-Webdev <collinshaya@gmail.com>
2024-09-29 13:30:15 +0100

    success

--- src/com/amazon/ata/hashingset/DeliveryDrone.java
+++ src/com/amazon/ata/hashingset/DeliveryDrone.java
@@ -1,40 +1,36 @@
 package com.amazon.ata.hashingset;
 
-/**
- * An Amazon Delivery Drone.
- */
+import java.util.Objects;
+
 public class DeliveryDrone {
-    
+
     private String registrationNumber;
     private String locationCode;
 
-    /**
-     * Construct an object representing an Amazon Delivery Drone.
-     *
-     * @param registrationNumber the registration number of this drone.
-     * @param locationCode the location identifier of the jurisdiction that registered the drone.
-     */
     public DeliveryDrone(String registrationNumber, String locationCode) {
         this.registrationNumber = registrationNumber;
         this.locationCode = locationCode;
     }
 
-    /**
-     * Return the registration number of this drone, as granted by
-     *   the relevant jurisdiction.
-     * @return the registration number of this drone.
-     */
     public String getRegistrationNumber() {
         return registrationNumber;
     }
 
-    /**
-     * Return the location code of the jurisdiction where the drone is registered.
-     *
-     * @return the jurisdiction location code.
-     */
     public String getLocationCode() {
         return locationCode;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeliveryDrone that = (DeliveryDrone) o;
+        return Objects.equals(registrationNumber, that.registrationNumber) &&
+                Objects.equals(locationCode, that.locationCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(registrationNumber, locationCode);
+    }
 }
